ac5ed850aaf8f0cd58764bea4f8709bb5ac0ad83
kristupas <sileikis.kristupas@gmail.com>
2024-09-12 17:10:50 +0300

    Create function to remove specific elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(numArray, ...numbers) {
+    for(let i = 0; i < numbers.length; i++){
+        for(let j = 0; j < numArray.length; j++){
+            if(numArray[j] == numbers[i] && typeof(numbers[i]) == typeof(numArray[j])){
+                numArray.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return numArray;
 };
 
 // Do not edit below this line
