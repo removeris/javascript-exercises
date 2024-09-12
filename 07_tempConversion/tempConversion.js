1d9e3151566d61ce009ffe8845d988a4ca8a6910
kristupas <sileikis.kristupas@gmail.com>
2024-09-12 18:08:49 +0300

    Create functions to convert celsius > fahrenheit and fahrenheit > celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegrees) {
+  // Formula for conversion - C = (F - 32) * 5/9)
+  const celsius = (fahrenheitDegrees - 32) * 5 / 9;
+
+  return Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  // Formula for conversion - F = (C * 9/5) + 32
+  const fahrenheit = (celsiusDegrees * 9 / 5) + 32;
+  
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
