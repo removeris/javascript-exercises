f4f0cbe28cccc7218c72fba5005e60a3a1fecb44
kristupas <sileikis.kristupas@gmail.com>
2024-09-12 17:32:59 +0300

    Create function to sum all elements in range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
 
+    if(start < 0 || finish < 0)
+        return "ERROR";
+    if(!Number.isInteger(start) || !Number.isInteger(finish))
+        return "ERROR";
+
+    if(start > finish){
+        let temp = start;
+        start = finish;
+        finish = temp;
+    }
+    
+    let sum = 0;
+    
+    for(; start <= finish; start++){
+        sum += start;
+    }
+    return sum;
 };
 
 // Do not edit below this line
