19c3e713d5953377a024c2dcaaa24097a58b79d3
kristupas <sileikis.kristupas@gmail.com>
2024-09-12 15:46:52 +0300

    Create function to repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    
+    if(count < 0)
+        return "ERROR";
 
+    let newString = '';
+
+    for(let i = 0; i < count; i++){
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
