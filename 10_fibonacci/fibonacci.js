c98ad641789600f10d26aa1692a7a4ced761f152
kristupas <108592864+removeris@users.noreply.github.com>
2024-09-16 14:59:02 +0300

    Create function to return n'th element of fibonacci seq.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+index = parseInt(index);
+
+if(index === 0)
+    return 0;
+if(index < 0)
+    return "OOPS";
+
+console.log(typeof(index));
+
+let sequence = [1, 1];
+
+for(let i = 2; i < index; i++){
+    sequence[i] = sequence[i - 2] + sequence[i - 1];
+}
+
+return sequence[index - 1];
 };
 
 // Do not edit below this line
