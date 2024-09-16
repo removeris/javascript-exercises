883b76ac65f8a833e225d2d5d80539036c1e4387
kristupas <108592864+removeris@users.noreply.github.com>
2024-09-16 15:36:21 +0300

    Create function to return the oldest person object from array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,36 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+/*
+people {
+    name:
+    yearOfBirth:
+    yearOfDeath:
+}*/
+
+let oldestPerson = people[0];
+
+if(!("yearOfDeath" in oldestPerson)){
+    oldestPerson.yearOfDeath = parseInt(new Date().getFullYear());
+}
+
+people.forEach((person) => {
+
+    if(!("yearOfDeath" in person)){
+        console.log("Hello");
+        person.yearOfDeath = new Date().getFullYear();
+    }
+    const currentPersonAge = person.yearOfDeath - person.yearOfBirth;
+    const oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+
+    console.log(`Current Oldest: ${oldestPerson.name} ${oldestPersonAge}`);
+
+    if(currentPersonAge > oldestPersonAge)
+        oldestPerson = person;
+})
+
+console.log(oldestPerson);
+
+return oldestPerson;
 };
 
 // Do not edit below this line
