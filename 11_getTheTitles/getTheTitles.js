f6595c18e36786661fe25fa5a08d3ec10e8c3c4a
kristupas <108592864+removeris@users.noreply.github.com>
2024-09-16 15:07:49 +0300

    Create function to extract array of titles from Obj array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
 
+    let titleArray = [];
+
+    bookArray.forEach((book) => {
+        titleArray.push(book.title);
+    })
+
+    return titleArray;
 };
 
 // Do not edit below this line
